e6f077592674de24b631355b472f8d4b4b576ca5
ArushKshatriya <arush.kshatriya@gmail.com>
2026-06-20 20:53:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
+    // Redirect TanStack Start's bundled server entry to src/server.ts
     server: { entry: "server" },
-    nitro: {
-      preset: "cloudflare-pages"
-    }
-  }
+  },
+  // Add this property to force-enable Nitro's deployment compilation target
+  nitro: true,
 });
